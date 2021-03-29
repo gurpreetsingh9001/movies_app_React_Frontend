574ccd6cdd8d72ebfc0883b1e64040c200670303
gurpreetsingh9001 <gpelia001@outlook.com>
2021-03-29 10:09:43 +0530

    deployment service

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,30 +1,29 @@
 import http from "./httpService";
-import { apiUrl } from "../config.json";
 
-const apiEndpoint = apiUrl + "/movies";
+const apiEndpoint = "/movies";
 
 function movieUrl(id) {
-  return `${apiEndpoint}/${id}`;
+    return `${apiEndpoint}/${id}`;
 }
 
 export function getMovies() {
-  return http.get(apiEndpoint);
+    return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(movieUrl(movieId));
+    return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
-  if (movie._id) {
-    const body = { ...movie };
-    delete body._id;
-    return http.put(movieUrl(movie._id), body);
-  }
+    if (movie._id) {
+        const body = { ...movie };
+        delete body._id;
+        return http.put(movieUrl(movie._id), body);
+    }
 
-  return http.post(apiEndpoint, movie);
+    return http.post(apiEndpoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(movieUrl(movieId));
+    return http.delete(movieUrl(movieId));
 }
